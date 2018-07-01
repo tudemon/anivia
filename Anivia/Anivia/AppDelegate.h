da82ec5c31d753e0838a712906aee0fc218e5454
erosennin <tuduonqquyet@gmail.com>
2018-07-01 15:31:30 +0700

    Init Commit

---
+++ Anivia/Anivia/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Anivia
+//
+//  Created by Tu Duong Quyet on 7/1/18.
+//  Copyright © 2018 Tu Duong Quyet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
---
+++ Anivia/Anivia/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Anivia
+//
+//  Created by Tu Duong Quyet on 7/1/18.
+//  Copyright © 2018 Tu Duong Quyet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
